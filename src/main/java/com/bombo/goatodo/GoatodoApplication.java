f5e40445a641196d8ae8f970780cd4dfc8a727d2
Bombo <bombo96@chungwoon.ac.kr>
2023-07-18 15:24:08 +0900

    init : goatodo 프로젝트 초기화

---
+++ src/main/java/com/bombo/goatodo/GoatodoApplication.java
@@ -0,0 +1,13 @@
+package com.bombo.goatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatodoApplication.class, args);
+	}
+
+}
