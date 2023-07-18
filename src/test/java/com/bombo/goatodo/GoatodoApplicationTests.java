dd7c8e6b7eae760628896c76a58b6de8d2c4501c
Bombo <bombo96@chungwoon.ac.kr>
2023-07-18 14:48:39 +0900

    init : goatodo 프로젝트 초기화

---
+++ src/test/java/com/bombo/goatodo/GoatodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombo.goatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
