bb856e41589e9c1927b93da0fdc989d46a2eeb44
Bombo <bombo96@chungwoon.ac.kr>
2023-07-19 18:30:54 +0900

    refactor : member의 @Length 애노테이션을 가벼운 @Size로 변경

--- src/main/java/com/bombo/goatodo/domain/member/Member.java
+++ src/main/java/com/bombo/goatodo/domain/member/Member.java
@@ -5,11 +5,11 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Getter
@@ -27,7 +27,7 @@ public class Member extends BaseEntity {
     private Account account;
 
     @NotBlank(message = "회원의 닉네임은 null 이거나 공백 일 수 없습니다.")
-    @Length(min = 2, max = 15, message = "닉네임은 2글자 이상 15자 이내여야 합니다.")
+    @Size(min = 2, max = 15, message = "닉네임은 2글자 이상 15자 이내여야 합니다.")
     @Column(name = "nickname", length = 15, nullable = false, unique = true)
     private String nickname;
 
