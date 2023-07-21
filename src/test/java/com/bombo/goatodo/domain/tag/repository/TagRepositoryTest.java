615e0e4060fd9e9a31c35cd335f74883be71d9f2
Bombo <bombo96@chungwoon.ac.kr>
2023-07-22 01:00:08 +0900

    refactor : testDB에 의존하지 않도록 변경

--- src/test/java/com/bombo/goatodo/domain/tag/repository/TagRepositoryTest.java
+++ src/test/java/com/bombo/goatodo/domain/tag/repository/TagRepositoryTest.java
@@ -13,15 +13,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class TagRepositoryTest {
 
