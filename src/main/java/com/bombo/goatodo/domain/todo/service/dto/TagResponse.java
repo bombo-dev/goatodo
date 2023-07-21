cd2233a1fd63415bf0bb693992f5d1a65e7f0f2c
Bombo <bombo96@chungwoon.ac.kr>
2023-07-22 00:58:07 +0900

    refactor : 유저가 수행하는 반환 값에 memberId 제거

--- src/main/java/com/bombo/goatodo/domain/todo/service/dto/TagResponse.java
+++ src/main/java/com/bombo/goatodo/domain/todo/service/dto/TagResponse.java
@@ -5,12 +5,11 @@ import com.bombo.goatodo.domain.todo.TagType;
 
 public record TagResponse(
         Long id,
-        Long memberId,
         String name,
         TagType tagType
 ) {
 
     public TagResponse(Tag tag) {
-        this(tag.getId(), tag.getMemberId(), tag.getName(), tag.getTagType());
+        this(tag.getId(), tag.getName(), tag.getTagType());
     }
 }
