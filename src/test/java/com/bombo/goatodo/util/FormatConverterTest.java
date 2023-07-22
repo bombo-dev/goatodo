5cdb25ae688da7e57c13cc53147447ac3b899ab6
Bombo <bombo96@chungwoon.ac.kr>
2023-07-22 13:55:01 +0900

    test : LocalDateTime을 yyyy-mm-dd HH:mm:ss 형태의 String으로 반환하는 테스트 추가

---
+++ src/test/java/com/bombo/goatodo/util/FormatConverterTest.java
@@ -0,0 +1,24 @@
+package com.bombo.goatodo.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+class FormatConverterTest {
+
+    @Test
+    @DisplayName("LocalDateTime을 yyyy-MM-dd HH:mm:ss 형태로 변환할 수 있다.")
+    void convertLocalDateTimeToString() {
+        // given
+        LocalDateTime localDateTime = LocalDateTime.of(2023, 7, 22, 13, 30, 22);
+
+        // when
+        String convertedLocalDateTime = FormatConverter.convertLocalDateTimeToString(localDateTime);
+
+        // then
+        Assertions.assertThat(convertedLocalDateTime).isEqualTo("2023-07-22 13:30:22");
+    }
+
+}
