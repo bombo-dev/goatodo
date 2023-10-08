ebefc6bf955368da68c06e844db38d0d5516d374
Bombo <bombo96@chungwoon.ac.kr>
2023-10-09 00:13:46 +0900

    refactor : Account 테스트로 이름을 변경

--- domain/src/test/java/com/goatodo/domain/user/AccountTest.java
+++ domain/src/test/java/com/goatodo/domain/user/AccountTest.java
@@ -1,121 +1,37 @@
 package com.goatodo.domain.user;
 
-import jakarta.validation.ConstraintViolation;
+import com.goatodo.domain.user.exception.InvalidEmailOrPasswordException;
 import org.assertj.core.api.Assertions;
-import org.hibernate.validator.HibernateValidator;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.junit.jupiter.api.Test;
 
-import java.util.Set;
+public class AccountTest {
 
-class AccountTest {
+    @Test
+    @DisplayName("회원 계정 정보의 아이디만 일치하고 비밀번호가 일치하지 않으면 같은 계정이 아니다.")
+    void notSameMemberWithDifferentEmailAndPasswordTest() {
+        // given
+        Account accountA = UserFactory.account("email@naver.com", "password1234");
+        Account accountB = UserFactory.account("email@naver.com", "password123");
 
-    private static LocalValidatorFactoryBean validator;
+        User user = UserFactory.user(accountA);
 
-    @BeforeAll
-    static void beforeAll() {
-        validator = new LocalValidatorFactoryBean();
-        validator.setProviderClass(HibernateValidator.class);
-        validator.afterPropertiesSet();
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+            user.validLoginInfo(accountB);
+        }).isInstanceOf(InvalidEmailOrPasswordException.class);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"email", "email@email", "email.com", "", " "})
-    @DisplayName("계정 Email은 Email 표기법에 맞게 작성되지 않는다면 위반된 검증사항이 존재한다.")
-    void accountEmailNotMatchPatternTest(String inputEmail) {
+    @Test
+    @DisplayName("회원 계정 정보의 아이디와 비밀번호가 일치하면 같은 회원이다.")
+    void SameMemberTest() {
         // given
-        Account account = Account.builder()
-                .email(inputEmail)
-                .password("abcd1234")
-                .build();
+        Account accountA = UserFactory.account("email@naver.com", "password1234");
+        User user = UserFactory.user(accountA);
 
-        // when
-        Set<ConstraintViolation<Account>> constraintViolation = validator.validate(account);
-
-        // then
-        Assertions.assertThat(constraintViolation).isNotEmpty();
-
-        Assertions.assertThat(constraintViolation)
-                .extracting(ConstraintViolation::getMessage)
-                .containsOnly("적절한 이메일 양식대로 입력해주세요. ex) goatodo@example.com");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"email@email.com", "email@email.net", "email@email.co.kr", "email@email.edu"})
-    @DisplayName("계정 Email은 Email 표기법에 맞게 작성되면 위반된 검증사항이 존재하지 않는다.")
-    void accountEmailValidationSuccess(String inputEmail) {
-        // given
-        Account account = Account.builder()
-                .email(inputEmail)
-                .password("abcd1234")
-                .build();
-
-        // when
-        Set<ConstraintViolation<Account>> validatedSet = validator.validate(account);
-
-        // then
-        Assertions.assertThat(validatedSet).isEmpty();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abcd123", "abcdefghijk1234567890", "", " "})
-    @DisplayName("계정 password의 길이가 적절한 8자 미만 20자 초과라면 위반된 검증사항이 존재한다.")
-    void accountPasswordOutOfRangeTest(String inputPassword) {
-        // given
-        Account account = Account.builder()
-                .email("goatodo@example.com")
-                .password(inputPassword)
-                .build();
-
-        // when
-        Set<ConstraintViolation<Account>> constraintViolation = validator.validate(account);
-
-        // then
-        Assertions.assertThat(constraintViolation).isNotEmpty();
-
-        Assertions.assertThat(constraintViolation)
-                .extracting(ConstraintViolation::getMessage)
-                .containsOnly("영문자+숫자, 8자 이상 20자 이내이여야 합니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abcdefgh", "12345678"})
-    @DisplayName("계정 password가 영문+숫자 조합이 아니라면 위반된 검증사항이 존재한다.")
-    void accountPasswordNotMathPatternTest(String inputPassword) {
-        // given
-        Account account = Account.builder()
-                .email("goatodo@example.com")
-                .password(inputPassword)
-                .build();
-
-        // when
-        Set<ConstraintViolation<Account>> constraintViolation = validator.validate(account);
-
-        // then
-        Assertions.assertThat(constraintViolation).isNotEmpty();
-
-        Assertions.assertThat(constraintViolation)
-                .extracting(ConstraintViolation::getMessage)
-                .containsOnly("영문자+숫자, 8자 이상 20자 이내이여야 합니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abcd1234", "abcdefghij1234567890", "password1234", "Password1234", "q1w2e3r4!@"})
-    @DisplayName("계정 password가 영문 + 숫자 조합이고 8자 이상 20자 이내면 위반된 검증사항이 존재하지 않는다.")
-    void accountPasswordPatternValidationSuccess(String inputPassword) {
-        // given
-        Account account = Account.builder()
-                .email("goatodo@example.com")
-                .password(inputPassword)
-                .build();
-
-        // when
-        Set<ConstraintViolation<Account>> validatedSet = validator.validate(account);
-
-        // then
-        Assertions.assertThat(validatedSet).isEmpty();
+        // when & then
+        Assertions.assertThatCode(() -> {
+            user.validLoginInfo(accountA);
+        }).doesNotThrowAnyException();
     }
 }
