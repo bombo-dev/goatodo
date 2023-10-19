9d17fa0efe35206298e77708444fb2f2916625d1
Bombo <bombo96@chungwoon.ac.kr>
2023-10-20 01:28:02 +0900

    chore : 멀티 모듈을 위한 설정을 변경하고, componentScan 위치를 통일

--- new_goatodo/goatodo/web/src/main/java/com/goatodo/web/WebApplication.java
+++ new_goatodo/goatodo/web/src/main/java/com/goatodo/web/WebApplication.java
@@ -2,8 +2,9 @@ package com.goatodo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.goatodo"})
 public class WebApplication {
 
     public static void main(String[] args) {
