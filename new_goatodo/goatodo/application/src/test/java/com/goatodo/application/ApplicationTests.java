e5a278650534a4878734c6b9e8ca28fd06465865
Bombo <bombo96@chungwoon.ac.kr>
2023-10-23 13:18:00 +0900

    chore : Applicaiton 영역을 테스트 할 수 있도록 필요한 애노테이션을 추가

--- new_goatodo/goatodo/application/src/test/java/com/goatodo/application/ApplicationTests.java
+++ new_goatodo/goatodo/application/src/test/java/com/goatodo/application/ApplicationTests.java
@@ -1,9 +1,14 @@
 package com.goatodo.application;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootTest
+@SpringBootApplication
+@EntityScan(basePackages = {"com.goatodo.domain"})
+@EnableJpaRepositories(basePackages = {"com.goatodo.domain"})
 class ApplicationTests {
 
     @Test
